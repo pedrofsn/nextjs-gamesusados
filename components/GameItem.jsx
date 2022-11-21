ac4079a7f34f9f21995c5cdc3602ef1d9598cdb2
pedrofsn <pedrokra@gmail.com>
2022-11-20 21:33:35 -0300

    Improving UI

--- components/GameItem.jsx
+++ components/GameItem.jsx
@@ -4,6 +4,6 @@ export default function GameItem(props) {
     const game = props.game
     return <div key={game.id} className={styles.game}>
         <img src={game.image} alt={game.title} className={styles.image}/>
-        {game.title}
+        <p>{game.title}</p>
     </div>
 }
