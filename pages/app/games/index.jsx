cce9305849e9c2983200c81419e9b1ac6aaae7d8
pedrofsn <pedrokra@gmail.com>
2022-11-20 22:35:12 -0300

    Using searchbox to call endpoint filtering by game title

--- pages/app/games/index.jsx
+++ pages/app/games/index.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import GameItem from '../../../components/GameItem.jsx'
+import Searchbox from "../../../components/Searchbox.jsx"
 import styles from '../../../styles/Games.module.css'
 
 export default function games() {
     const [games, setGames] = useState(null)
 
-    async function call() {
-        const response = await fetch('http://localhost:8080/games')
+    async function call(text = "") {
+        const url = text == "" ? "http://localhost:8080/games" : "http://localhost:8080/games?title=" + text
+        const response = await fetch(url)
         const json = await response.json()
         setGames(json)
     }
@@ -17,5 +19,12 @@ export default function games() {
     
     useEffect(() => { call() }, [])
 
-    return <div className={styles.page}>{generateList()}</div>
+    function onSearchTyped(text) {
+        call(text)
+    }
+
+    return <>
+        <Searchbox onSearchTyped={onSearchTyped}/>
+        <div className={styles.page}>{generateList()}</div>
+    </>
 }
