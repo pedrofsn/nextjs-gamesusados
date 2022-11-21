1d7aabe0d614d8168ec93ea4ee06ac52e5edee28
pedrofsn <pedrokra@gmail.com>
2022-11-20 21:30:01 -0300

    Creating component GameItem

---
+++ components/GameItem.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/GameItem.module.css'
+
+export default function GameItem(props) {
+    const game = props.game
+    return <div key={game.id} className={styles.game}>
+        <img src={game.image} alt={game.title} className={styles.image}/>
+        {game.title}
+    </div>
+}
---
+++ pages/app/games/games.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+import GameItem from '../../..//components/GameItem.jsx'
+import styles from '../../../styles/Games.module.css'
+
+export default function games() {
+    const [games, setGames] = useState(null)
+
+    async function call() {
+        const response = await fetch('http://localhost:8080/games')
+        const json = await response.json()
+        setGames(json)
+    }
+
+    function generateList() {
+        return games?.content?.map((game) => { return <GameItem key={game.id} game={game}/> })
+    }
+    
+    useEffect(() => { call() }, [])
+
+    return <div className={styles.page}>{generateList()}</div>
+}
