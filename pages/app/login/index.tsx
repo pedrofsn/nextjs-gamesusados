1154c20f4ca656b9b562d9c677d565582105c5af
pedrofsn <pedrokra@gmail.com>
2022-12-19 23:53:23 -0300

    starting to create login page

--- pages/app/login/index.tsx
+++ pages/app/login/index.tsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from "react"
-import { Input, Spacer } from "@nextui-org/react";
-import GameItem from '../../../components/GameItem.jsx'
-import { Button } from '@nextui-org/react';
+import { useState } from "react"
+import { Container, Input, Spacer, Button } from "@nextui-org/react";
 
 export default function games() {
-    const [games, setGames] = useState(null)
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
 
-    async function call(text = "") {
-        const url = text == "" ? "http://localhost:8080/games" : "http://localhost:8080/games?title=" + text
-        const response = await fetch(url)
-        const json = await response.json()
-        setGames(json)
-    }
-
-    function generateList() {
-        return games?.content?.map((game) => { return <GameItem key={game.id} game={game}/> })
-    }
-    
-    useEffect(() => { /*call()*/ }, [])
-
-    function onSearchTyped(text) {
-        call(text)
-    }
-
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
-
-    return <div className={`flex flex-col mtt-7`}>
+    return <Container xs>
+        <Input clearable label="login" placeholder="login" fullWidth={true} value={login} />
         <Spacer y={1.5} />
-        <Input clearable label="login" placeholder="login" />
+        <Input.Password clearable label="password" placeholder="password" fullWidth={true} value={password} />
         <Spacer y={1.5} />
         <Button>Click me</Button>
-    </div>
+    </Container>
 }
