427965275f92e6e2bd94b1bec8781ff781dd300b
pedrofsn <pedrokra@gmail.com>
2023-01-07 22:38:24 -0300

    Using Link component from NextJS and showing user type (development mode only

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,11 +1,21 @@
 import styles from '../styles/Searchbox.module.css'
-import { useState } from 'react'
+import useAppData from "../data/hook/useAppData"
+import { Text } from "@nextui-org/react";
+import Link from 'next/link';
 
 export default function Searchbox(props) {
+    const { systemSession } = useAppData()
+
+    const userType = systemSession.userSession.usertype
+
     return <div className={styles.topnav}>
-        <a className={styles.active} href="#games">Games</a>
-        <a href="#about">Sobre</a>
+        <Text h3>Olá, {userType}!</Text>
+        <Link href="games">Games</Link>
+        <Link href="announcement/pending">Anúncios pendentes</Link>
         <a href="#contact">Contato</a>
-        <input type="text" placeholder="Digite o nome do jogo.." onChange={(e) => props.onSearchTyped(e.target.value)} />
+        <input type="text"
+            placeholder="Digite o nome do jogo.."
+            onChange={(e) => props.onSearchTyped(e.target.value)}
+        />
     </div>
 }
