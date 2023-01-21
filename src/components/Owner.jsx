9253315be41b01c0b6547a5d454e72cbbe9cc15a
pedrofsn <pedrokra@gmail.com>
2023-01-21 11:17:05 -0300

    Removing unused code

--- src/components/Owner.jsx
+++ src/components/Owner.jsx
@@ -1,19 +1,12 @@
-import { Card, User, Text, Spacer } from "@nextui-org/react";
+import { User } from "@nextui-org/react";
 
 export default function Owner(props) {
   const { owner } = props
-  console.log('owner : ' + JSON.stringify(owner))
-  return <>
-    {/* <Card.Divider />
-    <Card.Body> */}
-    <User
-      name={owner.name}
-      description={owner.email}
-      text={owner.name[0]}
-      size="xl"
-      squared
-    />
-    {/* <Spacer y={1.5} />
-    </Card.Body> */}
-  </>;
+  return <User
+    name={owner.name}
+    description={owner.email}
+    text={owner.name[0]}
+    size="xl"
+    squared
+  />;
 }
