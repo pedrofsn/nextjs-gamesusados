836bda085ef462f35114ff9ce6d1c957832b1832
pedrofsn <pedrokra@gmail.com>
2023-03-06 15:00:36 -0300

    Migrating to TS & applying right usage of app ctx

--- src/pages/app/games/index.tsx
+++ src/pages/app/games/index.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react"
 import GameItem from '../../../components/GameItem.jsx'
 import Searchbox from "../../../components/Toolbar"
-import { Col, Grid, Row, Container } from "@nextui-org/react";
+import { Col, Grid, Row, Container } from "@nextui-org/react"
 import { api } from '../../../services/api'
-import RegisterGame from "../../../components/RegisterGame/RegisterGame";
+import RegisterGame from "../../../components/RegisterGame/RegisterGame"
+import useAppData from "../../../data/hook/useAppData"
+import { UserType } from "../../../data/context/UserSession"
 
 export default function games() {
     const [games, setGames] = useState(null)
+    const { systemSession } = useAppData()
+    const userType: UserType = systemSession.userSession.usertype
 
     async function call(text = "") {
         const json = await api({
@@ -39,19 +43,25 @@ export default function games() {
 
     // TODO 'RegisterGame' só deve ser exibido se o user for do tipo MANAGER
 
+    const uiManager = <Container css={{ width: '100%' }}>
+        <Row css={{ width: '100%' }}>
+            <Col css={{ width: '25%' }}>
+                <RegisterGame onGameSaved={() => call()} />
+            </Col>
+            <Col css={{ width: '75%' }}>
+                <Grid.Container gap={1} justify="flex-start">
+                    {generateList()}
+                </Grid.Container>
+            </Col>
+        </Row>
+    </Container>
+
+    const uiNotManager = <Grid.Container gap={1} justify="flex-start">
+        {generateList()}
+    </Grid.Container>
+
     return <>
         <Searchbox onSearchTyped={onSearchTyped} />
-        <Container css={{ width: '100%' }}>
-            <Row css={{ width: '100%' }}>
-                <Col css={{ width: '25%' }}>
-                    <RegisterGame onGameSaved={() => call()} />
-                </Col>
-                <Col css={{ width: '75%' }}>
-                    <Grid.Container gap={1} justify="flex-start">
-                        {generateList()}
-                    </Grid.Container>
-                </Col>
-            </Row>
-        </Container>
+        {userType == 'MANAGER' ? uiManager : uiNotManager}
     </>
 }
