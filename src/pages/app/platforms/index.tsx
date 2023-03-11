b9a3f605c9def366675391c2c41a8e3c192c4c67
pedrofsn <pedrokra@gmail.com>
2023-03-11 12:55:00 -0300

    just showing register platform when user logged is MANAGER

--- src/pages/app/platforms/index.tsx
+++ src/pages/app/platforms/index.tsx
@@ -3,9 +3,12 @@ import { Table, Container, Row, Col } from "@nextui-org/react";
 import { api } from '../../../services/api'
 import Searchbox from "../../../components/Toolbar";
 import RegisterPlatform from "../../../components/RegisterPlatform";
+import { parseCookies } from "nookies"
+import { UserType } from "../../../data/context/UserSession.jsx"
 
 export default function platforms() {
     const [content, setContent] = useState(null)
+    const [userType, setUserType] = useState<UserType>(null)
 
     function onRegistered(text) {
         call()
@@ -16,7 +19,11 @@ export default function platforms() {
         setContent(json.data)
     }
 
-    useEffect(() => { call() }, [])
+    useEffect(() => {
+        const { 'gamesusados.usertype': userType } = parseCookies()
+        setUserType(userType as UserType || 'USER')
+        call()
+    }, [userType])
 
     function getTable() {
         return content?.map((element) => {
@@ -29,25 +36,29 @@ export default function platforms() {
         })
     }
 
+    const uiNotManager = <Table>
+        <Table.Header>
+            <Table.Column>Id</Table.Column>
+            <Table.Column>Plataforma</Table.Column>
+        </Table.Header>
+        <Table.Body>
+            {getTable()}
+        </Table.Body>
+    </Table>
+
+    const uiManager = <Container css={{ width: '100%' }}>
+        <Row css={{ width: '100%' }}>
+            <Col css={{ width: '25%' }}>
+                <RegisterPlatform onRegistered={onRegistered} />
+            </Col>
+            <Col css={{ width: '75%' }}>
+                {uiNotManager}
+            </Col>
+        </Row>
+    </Container>
+
     return <>
         <Searchbox />
-        <Container css={{ width: '100%' }}>
-            <Row css={{ width: '100%' }}>
-                <Col css={{ width: '25%' }}>
-                    <RegisterPlatform onRegistered={onRegistered} />
-                </Col>
-                <Col css={{ width: '75%' }}>
-                    <Table>
-                        <Table.Header>
-                            <Table.Column>Id</Table.Column>
-                            <Table.Column>Plataforma</Table.Column>
-                        </Table.Header>
-                        <Table.Body>
-                            {getTable()}
-                        </Table.Body>
-                    </Table>
-                </Col>
-            </Row>
-        </Container>
+        {userType == 'MANAGER' ? uiManager : uiNotManager}
     </>
 }
